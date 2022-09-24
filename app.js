3768eea18f1e013d151c9a757410ef67dbfbf355
Carlos-Pujols <carlospujols94@gmail.com>
2022-09-23 23:58:50 -0400

    Modified structures to help info loading

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
-var dailyButton = document.getElementById('daily');
+const monthlyButton = document.getElementById('monthly');
+const weeklyButton = document.getElementById('weekly');
+const dailyButton = document.getElementById('daily');
+
+var currentWork = document.getElementById('current-work');
+var previousWork = document.getElementById('previous-work');
+
+var currentPlay = document.getElementById('current-play');
+var previousPlay = document.getElementById('previous-play');
+
+var currentStudy = document.getElementById('current-study');
+var previousStudy = document.getElementById('previous-study');
+
+var currentExercise = document.getElementById('current-exercise');
+var previousExercise = document.getElementById('previous-exercise');
+
+var currentSocial = document.getElementById('current-social');
+var previousSocial = document.getElementById('previous-social');
+
+var currentSelfCare = document.getElementById('current-selfcare');
+var previousSelfCare = document.getElementById('previous-selfcare');
+
+window.addEventListener('load', defaultActions);
+
+function defaultActions(){
+
+    weeklyButton.style.color = "white";
+    weeklyButton.style.opacity = 100;
+    console.log('Page load defaults to weekly values');
+
+}
+
+
 
 dailyButton.addEventListener('click', loadDailyInfo);
 
@@ -7,19 +39,12 @@ function loadDailyInfo(){
 }
 
 
-
-var weeklyButton = document.getElementById('weekly');
-
 weeklyButton.addEventListener('click', loadWeeklyInfo);
 
 function loadWeeklyInfo(){
     console.log('Clicked Weekly Button');
 }
 
-
-
-
-var monthlyButton = document.getElementById('monthly');
 
 monthlyButton.addEventListener('click', loadMonthlyInfo);
 
